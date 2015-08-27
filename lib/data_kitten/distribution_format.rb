f8bd927f9f18fa76346914829208bd77e61028b6
Adam Sven Johnson <adam@pkqk.net>
2015-08-27 14:08:57 +0100

    Remove use of @@formats, move to module constant
    This was a strange way to store this information.
    
    Store known formats in a hash and set the default
    to {} so that lookups don’t need to protect
    against unknown formats.

--- lib/data_kitten/distribution_format.rb
+++ lib/data_kitten/distribution_format.rb
@@ -6,6 +6,30 @@ module DataKitten
   #
   class DistributionFormat
 
+    FORMATS = {
+      csv: { structured: true, open: true },
+      xls: { structured: true, open: false },
+      xlsx: { structured: true, open: true },
+      rdf: { structured: true, open: true },
+      xml: { structured: true, open: true },
+      wms: { structured: true, open: true },
+      ods: { structured: true, open: true },
+      rdfa: { structured: true, open: true },
+      kml: { structured: true, open: true },
+      rss: { structured: true, open: true },
+      json: { structured: true, open: true },
+      ical: { structured: true, open: true },
+      sparql: { structured: true, open: true },
+      kml: { structured: true, open: true },
+      georss: { structured: true, open: true },
+      geojson: { structured: true, open: true },
+      shp: { structured: true, open: true },
+      html: { structured: false, open: true },
+      doc: { structured: false, open: false },
+      pdf: { structured: false, open: true }
+    }
+    FORMATS.default = {}
+
     #@!attribute extension
     #@return [Symbol] a symbol for the file extension. For instance, :csv.
     attr_reader :extension
@@ -17,43 +41,20 @@ module DataKitten
       @distribution = distribution
       # Store extension as a lowercase symbol
       @extension = distribution.extension.to_s.downcase.to_sym
-      # Set up format lists
-      @@formats ||= {
-        csv:     { structured:  true, open:  true },
-        xls:     { structured:  true, open: false },
-        xlsx:    { structured:  true, open:  true },
-        rdf:     { structured:  true, open:  true },
-        xml:     { structured:  true, open:  true },
-        wms:     { structured:  true, open:  true },
-        ods:     { structured:  true, open:  true },
-        rdfa:    { structured:  true, open:  true },
-        kml:     { structured:  true, open:  true },
-        rss:     { structured:  true, open:  true },
-        json:    { structured:  true, open:  true },
-        ical:    { structured:  true, open:  true },
-        sparql:  { structured:  true, open:  true },
-        kml:     { structured:  true, open:  true },
-        georss:  { structured:  true, open:  true },
-        geojson: { structured:  true, open:  true },
-        shp:     { structured:  true, open:  true },
-        html:    { structured: false, open:  true },
-        doc:     { structured: false, open:  false },
-        pdf:     { structured: false, open:  true },
-      }
     end
 
     # Is this a structured format?
     #
     # @return [Boolean] whether the format is machine-readable or not.
     def structured?
-      @@formats[@extension][:structured] rescue nil
+      FORMATS[extension][:structured]
     end
 
     # Is this an open format?
     #
     # @return [Boolean] whether the format is open or not
     def open?
-      @@formats[@extension][:open] rescue nil
+      FORMATS[extension][:open]
     end
 
     # Whether the format of the file matches the extension given by the data
