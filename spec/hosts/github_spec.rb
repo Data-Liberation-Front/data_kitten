60eff6d89aaa9c0bcc18c01bd7d0551e99a891f1
Adam Sven Johnson <adam@pkqk.net>
2015-08-27 13:19:00 +0100

    Reduce duplication in `Hosts::Github` spec
      * Name the file after what it is testing
      * Parameterize the specs instead of copypasting

---
+++ spec/hosts/github_spec.rb
@@ -0,0 +1,21 @@
+require 'spec_helper'
+
+describe DataKitten::Dataset do
+
+  context 'with data on github' do
+
+    def access_url(protocol)
+      "#{protocol}://github.com/theodi/github-viewer-test-data.git"
+    end
+
+    %w[https http git].each do |protocol|
+      it "correctly identified #{protocol} URLs p" do
+        dataset = DataKitten::Dataset.new(
+          access_url: "#{protocol}://github.com/theodi/github-viewer-test-data.git")
+        expect(dataset.host).to eq(:github)
+      end
+    end
+
+  end
+
+end
