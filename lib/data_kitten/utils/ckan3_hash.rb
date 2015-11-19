e234db8d9e0ae36920d32edb353618c640221424
Adam Sven Johnson <adam@pkqk.net>
2015-11-19 16:17:06 +0000

    Simplify hash wrapper implementation

--- lib/data_kitten/utils/ckan3_hash.rb
+++ lib/data_kitten/utils/ckan3_hash.rb
@@ -1,19 +1,8 @@
 class CKAN3Hash < Hash
   def initialize(list, key_name, value_name)
     super()
-      self[item[key_name]] = item
     (list || []).each do |item|
+      self[item[key_name]] = item[value_name]
     end
-    @value_name = value_name
-  end
-
-  def [](key)
-    if value = super(key)
-      value[@value_name]
-    end
-  end
-
-  def values
-    super.map {|value| value[@value_name]}
   end
 end
