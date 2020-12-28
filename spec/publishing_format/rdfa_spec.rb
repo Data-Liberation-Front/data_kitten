201cc4ffd974ac61bd92660a462c29c4f9dccc24
James Smith <james@floppy.org.uk>
2020-12-28 10:49:11 +0000

    fix array ordering dependence in test

--- spec/publishing_format/rdfa_spec.rb
+++ spec/publishing_format/rdfa_spec.rb
@@ -5,92 +5,92 @@ describe DataKitten::PublishingFormats::RDFa do
     before(:each) do
         FakeWeb.clean_registry
     end
-    
+
     context "when detecting RDFa" do
-        
-        it "should ignore errors" do       
-            FakeWeb.register_uri(:get, "http://example.org/not-found", :status => ["404", "Not Found"])            
+
+        it "should ignore errors" do
+            FakeWeb.register_uri(:get, "http://example.org/not-found", :status => ["404", "Not Found"])
             d = DataKitten::Dataset.new("http://example.org/not-found")
-            expect( d.supported? ).to eql(false)        
+            expect( d.supported? ).to eql(false)
         end
 
         it "should detect DCAT Datasets" do
             dcat_rdfa = load_fixture("basic-dcat-rdfa.html")
-            FakeWeb.register_uri(:get, "http://example.org/rdfa", :body=>dcat_rdfa, :content_type=>"text/html")            
+            FakeWeb.register_uri(:get, "http://example.org/rdfa", :body=>dcat_rdfa, :content_type=>"text/html")
             d = DataKitten::Dataset.new("http://example.org/rdfa")
-            expect( d.publishing_format ).to eql(:rdfa)        
-            expect( d.supported? ).to eql(true)                    
+            expect( d.publishing_format ).to eql(:rdfa)
+            expect( d.supported? ).to eql(true)
         end
     end
-    
+
     context "when parsing RDFa" do
-        
+
         before(:each) do
             dcat_rdfa = load_fixture("basic-dcat-rdfa.html")
-            FakeWeb.register_uri(:get, "http://example.org/rdfa", :body=>dcat_rdfa, :content_type=>"text/html")            
+            FakeWeb.register_uri(:get, "http://example.org/rdfa", :body=>dcat_rdfa, :content_type=>"text/html")
             @dataset = DataKitten::Dataset.new("http://example.org/rdfa")
-        end        
-        
+        end
+
         it "should extract the title" do
-            expect( @dataset.data_title ).to eql("Example DCAT Dataset")              
+            expect( @dataset.data_title ).to eql("Example DCAT Dataset")
         end
 
         it "should extract the description" do
-            expect( @dataset.description ).to eql("This is the description.")              
+            expect( @dataset.description ).to eql("This is the description.")
         end
-        
+
         it "should extract licenses" do
             expect( @dataset.licenses.length ).to eql(1)
             licence = @dataset.licenses.first
             expect( licence.uri ).to eql("http://reference.data.gov.uk/id/open-government-licence")
-            expect( licence.name ).to eql("UK Open Government Licence (OGL)")            
+            expect( licence.name ).to eql("UK Open Government Licence (OGL)")
         end
-        
+
         it "should extract publisher details" do
             expect( @dataset.publishers.length ).to eql(1)
             publisher = @dataset.publishers.first
-            expect( publisher.name ).to eql("Example Publisher")   
-            expect( publisher.uri ).to eql("http://example.org/publisher")                                 
+            expect( publisher.name ).to eql("Example Publisher")
+            expect( publisher.uri ).to eql("http://example.org/publisher")
         end
-           
+
         it "should extract keywords" do
             expect( @dataset.keywords.length ).to eql(2)
-            expect( @dataset.keywords.first ).to eql("Examples")
-            expect( @dataset.keywords.last ).to eql("DCAT")
+            expect( @dataset.keywords ).to include("Examples")
+            expect( @dataset.keywords ).to include("DCAT")
         end
-        
+
         it "should extract update frequency" do
             expect( @dataset.update_frequency).to eql("http://purl.org/linked-data/sdmx/2009/code#freq-W")
         end
-    
+
         it "should extract distributions" do
             expect( @dataset.distributions.length).to eql(1)
-            
+
             distribution = @dataset.distributions.first
-            
+
             expect( distribution.title ).to eql("CSV download")
             expect( distribution.access_url).to eql("http://example.org/distribution.csv.zip")
         end
-        
+
         it "should extract dates" do
             expect( @dataset.issued.to_s ).to eql("2010-10-25")
             expect( @dataset.modified.to_s ).to eql("2013-05-10")
         end
     end
-    
+
     context "when parsing rights statements" do
-        
+
         before(:each) do
             dcat_rdfa = load_fixture("dcat-odrs-rdfa.html")
-            FakeWeb.register_uri(:get, "http://example.org/rights", :body=>dcat_rdfa, :content_type=>"text/html")            
+            FakeWeb.register_uri(:get, "http://example.org/rights", :body=>dcat_rdfa, :content_type=>"text/html")
             @dataset = DataKitten::Dataset.new("http://example.org/rights")
-        end        
-        
+        end
+
         it "should extract licence URIs" do
             @dataset.rights.dataLicense = "http://reference.data.gov.uk/id/open-government-licence"
             @dataset.rights.contentLicense = "http://reference.data.gov.uk/id/open-government-licence"
         end
-        
+
         it "should extract copyright information" do
             @dataset.rights.copyrightYear = "2013"
             @dataset.rights.databaseRightYear = "2013"
@@ -102,6 +102,6 @@ describe DataKitten::PublishingFormats::RDFa do
             @dataset.rights.attributionText = "Example Department"
             @dataset.rights.attributionURL = "http://gov.example.org/dataset/finances"
         end
-                
+
     end
-end    
+end
