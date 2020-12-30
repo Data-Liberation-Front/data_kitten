eca176843a00a51a0904a9ba8f195b7a1cc1e833
James Smith <james@floppy.org.uk>
2020-12-30 22:42:44 +0000

    lint: use snake case for ruby vars
    added aliases to preserve API

--- lib/data_kitten/rights.rb
+++ lib/data_kitten/rights.rb
@@ -6,49 +6,60 @@ module DataKitten
     #   @return [String] the URI for the rights statement
     attr_accessor :uri
 
-    # @!attribute dataLicense
+    # @!attribute data_license
     #   @return [String] the license for the data in the dataset.
-    attr_accessor :dataLicense
+    attr_accessor :data_license
+    alias dataLicense data_license
 
-    # @!attribute contentLicense
+    # @!attribute content_license
     #   @return [String] the license for the content in the dataset.
-    attr_accessor :contentLicense
+    attr_accessor :content_license
+    alias contentLicense content_license
 
     # @!attribute copyrightNotice
     #   @return [String] the copyright notice for the dataset.
-    attr_accessor :copyrightNotice
+    attr_accessor :copyright_notice
+    alias copyrightNotice copyright_notice
 
-    # @!attribute attributionURL
+    # @!attribute attribution_url
     #   @return [String] the attribution URL for the dataset.
-    attr_accessor :attributionURL
+    attr_accessor :attribution_url
+    alias attributionURL attribution_url
 
-    # @!attribute attributionText
+    # @!attribute attribution_text
     #   @return [String] the attribution text for the dataset.
-    attr_accessor :attributionText
+    attr_accessor :attribution_text
+    alias attributionText attribution_text
 
-    # @!attribute copyrightHolder
+    # @!attribute copyright_holder
     #   @return [String] the URI of the organization that holds copyright for this dataset
-    attr_accessor :copyrightHolder
+    attr_accessor :copyright_holder
+    alias copyrightHolder copyright_holder
 
-    # @!attribute databaseRightHolder
+    # @!attribute database_right_holder
     #   @return [String] the URI of the organization that owns the database rights for this dataset
-    attr_accessor :databaseRightHolder
+    attr_accessor :database_right_holder
+    alias databaseRightHolder database_right_holder
 
-    # @!attribute copyrightYear
+    # @!attribute copyright_year
     #   @return [String] the year in which copyright is claimed
-    attr_accessor :copyrightYear
+    attr_accessor :copyright_year
+    alias copyrightYear copyright_year
 
-    # @!attribute databaseRightYear
+    # @!attribute database_right_year
     #   @return [String] the year in which copyright is claimed
-    attr_accessor :databaseRightYear
+    attr_accessor :database_right_year
+    alias databaseRightYear database_right_year
 
-    # @!attribute copyrightStatement
+    # @!attribute copyright_statement
     #   @return [String] the URL of a copyright statement for the dataset
-    attr_accessor :copyrightStatement
+    attr_accessor :copyright_statement
+    alias copyrightStatement copyright_statement
 
-    # @!attribute databaseRightStatement
+    # @!attribute database_right_statement
     #   @return [String] the URL of a database right statement for the dataset
-    attr_accessor :databaseRightStatement
+    attr_accessor :database_right_statement
+    alias databaseRightStatement database_right_statement
 
     # Create a new Rights object.
     #
@@ -60,17 +71,17 @@ module DataKitten
     # @option options [String] :attributionText attribution name for the dataset
     def initialize(options)
       @uri = options[:uri]
-      @dataLicense = options[:dataLicense]
-      @contentLicense = options[:contentLicense]
-      @copyrightNotice = options[:copyrightNotice]
-      @attributionURL = options[:attributionURL]
-      @attributionText = options[:attributionText]
-      @copyrightHolder = options[:copyrightHolder]
-      @databaseRightHolder = options[:databaseRightHolder]
-      @copyrightYear = options[:copyrightYear]
-      @databaseRightYear = options[:databaseRightYear]
-      @copyrightStatement = options[:copyrightStatement]
-      @databaseRightStatement = options[:databaseRightStatement]
+      @data_license = options[:dataLicense]
+      @content_license = options[:contentLicense]
+      @copyright_notice = options[:copyrightNotice]
+      @attribution_url = options[:attributionURL]
+      @attribution_text = options[:attributionText]
+      @copyright_holder = options[:copyrightHolder]
+      @database_right_holder = options[:databaseRightHolder]
+      @copyright_year = options[:copyrightYear]
+      @database_right_year = options[:databaseRightYear]
+      @copyright_statement = options[:copyrightStatement]
+      @database_right_statement = options[:databaseRightStatement]
     end
   end
 end
